96e9a2921e0c8cefdf2389b57da1a73ab9e4ecc9
Andrew <mitchel_mit@mail.ru>
2021-12-02 14:52:05 +0300

    Added progress bar and instant termination in case of IP blocking.

--- main.py
+++ main.py
@@ -2,17 +2,19 @@ from urllib.request import urlretrieve
 import requests
 from bs4 import BeautifulSoup
 from fake_useragent import UserAgent
-import time
 import re
 import os
+import time
+import random
+from tqdm import tqdm
 
-URL = 'https://zastavok.net/1920x1080/'
+
+URL = 'https://zastavok.net/2560x1440/'
 HOST = 'https://zastavok.net'
 ua = UserAgent()
-HEADERS = {'User-Agent': ua.random}
 
 def get_html(url, params=None):
-    r = requests.get(url, headers=HEADERS, params=params)
+    r = requests.get(url, headers={'User-Agent': ua.random}, params=params)
     r.encoding = 'utf8'
     return r
 
@@ -34,15 +36,16 @@ def get_pic(html):
 
 
 def parsing():
+    exitFlag = False
     pages = int(input('Введите нужное количество страниц: ')) + 1
-    for i in range(1, pages):
+    for i in range(1+15, pages+1+15):
         html_all_pics = get_html(URL + str(i))
         print(f'Парсинг страницы {html_all_pics.url}...')
         if html_all_pics.ok:
             links = get_links(html_all_pics.text)
         else:
             print('Сайт не отвечает')
-        for link in links:
+        for link in tqdm(links):
             html_link_content = get_html(link)
             if html_link_content.ok:
                 pic_link = get_pic(html_link_content.text)
@@ -51,6 +54,7 @@ def parsing():
                     d = r.headers['content-disposition']
                 except:
                     print('Достигнут лимит скачивания на этом IP.')
+                    exitFlag = True
                     break
                 file_name = re.findall("filename=(.+)", d)[0].replace('"', '')
                 try:
@@ -62,9 +66,11 @@ def parsing():
                     print(f'Изображение {file_name} сохранено успешно.')
                 else:
                     print(f'Изображение {file_name} уже было скачано.')
-                time.sleep(3)
+                # time.sleep(random.randint(3, 9))
             else:
                 print('Сайт не отвечает')
+        if exitFlag:
+            break
 
 
 
